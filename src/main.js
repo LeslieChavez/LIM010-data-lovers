edf908b2c3a25bb454df6298367b480f66c2a9b0
AndreinaKj <anoherli@gmail.com>
2019-06-27 09:14:17 -0500

    Login completo. HTML bàsico y JS. Testeado. Falta CSS

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
 /* Manejo del DOM */
+const user = document.getElementById('user');
+const password = document.getElementById('password');
+const login = document.getElementById('login');
+const logOut = document.getElementById('error');
+const informationPage = document.getElementById('information');
+// Función para validar contraseña correcta
+const userTrue = 'LABORATORIA'; 
+const passwordTrue = 'LABORATORIA';
+let tryingNumb = 0;
+const validation = () => { 
+  if (password.value === passwordTrue && user.value === userTrue) {
+    login.classList.add('hide');
+    informationPage.classList.replace('hide', 'show');
+    event.preventDefault();
+  } else {
+    if (tryingNumb < 2) {
+      wrong.classList.replace('hide', 'show');
+      password.value = '';
+      tryingNumb++;
+      event.preventDefault();
+    } else {
+      login.classList.add('hide');
+      wrong.classList.add('hide');
+      error.classList.replace('hide', 'show');
+      event.preventDefault();
+    } 
+  }
+};  
+const loginButton = document.getElementById('enter-button');
+loginButton.addEventListener('click', validation);
