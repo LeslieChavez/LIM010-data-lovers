1f33c7647f889869b6377bcf823c2d468e840984
AndreinaKj <anoherli@gmail.com>
2019-06-28 08:41:38 -0500

    H1: Solo falta el readMe. HTML,JS y CSS listo

--- src/main.js
+++ src/main.js
@@ -1,18 +1,28 @@
 /* Manejo del DOM */
 
 const loginBg = document.getElementById('login-bg');
-const secondScreen = document.getElementById('secondScreen');
+const secondScreen = document.getElementById('second-screen');
 const password = document.getElementById('password');
 const user = document.getElementById('user');
-const errorMessage = document.getElementById('errorMessage');
-const enterButton = document.getElementById('enterButton');
+const enterButton = document.getElementById('enter-button');
+const errorMessage = document.getElementById('error-message');
 // Función para validar contraseña correcta
-enterButton.addEventListener ('click',() => {
-  if (user.value === "" && password.value === "") {
-    loginScreen.classList.replace(show, hide);
-    secondScreen.classList.replace(hide, show);
-  } else {
-    errorMessage.innerHTML = "Datos incorrectos"
-  }
-});
 
+// Definir variable para realizar función 
+const userTrue = 'LABORATORIA';
+const passwordTrue = 'LABORATORIA';
+let tryNumb = 0;
+
+// Función para validar contraseña correcta
+const validation = () => { 
+  const password = document.getElementById('password');
+  if (password.value === passwordTrue) {
+    loginBg.classList.add('hide');
+    secondScreen.classList.replace('hide', 'show');
+    event.preventDefault();
+  } else {
+    loginBg.classList.add('hide');
+    errorMessage.classList.add('hide');
+  }
+}; 
+enterButton.addEventListener('click', validation);
