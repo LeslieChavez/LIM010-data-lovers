aabdb561a6bc1e026f216b2c3245f6c11ac07f61
AndreinaKj <anoherli@gmail.com>
2019-07-18 20:35:47 -0500

    arreglando y probando github. hay que arreglar el modal

--- src/data.js
+++ src/data.js
@@ -1,8 +1,3 @@
-/* dato: función
-  p1: data => array,
-  p2: wanted => string que representa el nombre del pokemon
-*/
-
 /* Ordenar Alfabeticamente*/
 const sortAlfa = (data) => {
   return data.sort((prev, next) => {
@@ -12,18 +7,18 @@ const sortAlfa = (data) => {
     if (prev.name < next.name) {
       return -1;
     };
-    return 0;
+    return 0; /* Falta hacer */
   });
-};
-
-/* Ordenar por Aparición */
-const sortSpawn = (data) => {
-  return data.sort((prev, next) => prev.spawn_chance - next.spawn_chance);
 };
 
 /* Buscar a un pokémon */
 const searchPokemons = (data, wanted) => {
   return data.filter(obj => obj.name.toLowerCase().startsWith(wanted));
+};
+
+/* Ordenar por Aparición */
+const sortSpawn = (data) => {
+  return data.sort((prev, next) => prev.spawn_chance - next.spawn_chance);
 };
 
 /* Filtra por debilidad */
@@ -41,16 +36,13 @@ const filterEggs = (data, typeEgg) => {
   return data.filter(obj => obj.egg.indexOf(typeEgg) > -1);
 };
 
-window = {
-  searchPokemons: searchPokemons,
-  sortAlfa: sortAlfa,
-  sortSpawn: sortSpawn,
-  filterWeakness: filterWeakness,
-  filterTypes: filterTypes,
-  filterEggs: filterEggs
-  /* catchedPokemon: catchedPokemon,
-  unCatchedPokemon: unCatchedPokemon,*/
-};
+window.searchPokemons = searchPokemons;
+window.sortSpawn = sortSpawn;
+window.sortAlfa = sortAlfa;
+window.filterWeakness = filterWeakness;
+window.filterTypes = filterTypes;
+window.filterEggs = filterEggs;
+
 
 /* Pokemones atrapados / no atrapados 
 const catchedPokemon = (data) => {
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,15 +1,290 @@
 global.window = global;
-global.assert = require('chai').assert;
+// global.assert = require('chai').assert;
 require('../src/data');
 require('./data.spec.js');
 
+const data = [
+  {
+    'id': 41,
+    'num': '041',
+    'name': 'Zubat',
+    'img': 'http://www.serebii.net/pokemongo/pokemon/041.png',
+    'type': [
+      'Poison',
+      'Flying'
+    ],
+    'egg': '2 km',
+    'spawn_chance': 6.52,
+    'avg_spawns': 652,
+    'spawn_time': '12:28',
+    'multipliers': [
+      2.6,
+      3.67
+    ],
+    'weaknesses': [
+      'Electric',
+      'Ice',
+      'Psychic',
+      'Rock'
+    ]
+  }, {
+    'id': 42,
+    'num': '042',
+    'name': 'Golbat',
+    'img': 'http://www.serebii.net/pokemongo/pokemon/042.png',
+    'type': [
+      'Poison',
+      'Flying'
+    ],
+    'egg': 'Not in Eggs',
+    'spawn_chance': 0.42,
+    'avg_spawns': 42,
+    'spawn_time': '02:15',
+    'multipliers': null,
+    'weaknesses': [
+      'Electric',
+      'Ice',
+      'Psychic',
+      'Rock'
+    ],
+    'prev_evolution': [{
+      'num': '041',
+      'name': 'Zubat'
+    }]
+  }, {
+    'id': 145,
+    'num': '145',
+    'name': 'Zapdos',
+    'img': 'http://www.serebii.net/pokemongo/pokemon/145.png',
+    'type': [
+      'Electric',
+      'Flying'
+    ],
+    'egg': 'Not in Eggs',
+    'spawn_chance': 0,
+    'avg_spawns': 0,
+    'spawn_time': 'N/A',
+    'multipliers': null,
+    'weaknesses': [
+      'Ice',
+      'Rock'
+    ]
+  }, {
+    'id': 144,
+    'num': '144',
+    'name': 'Articuno',
+    'img': 'http://www.serebii.net/pokemongo/pokemon/144.png',
+    'type': [
+      'Ice',
+      'Flying'
+    ],
+    'egg': 'Not in Eggs',
+    'spawn_chance': 0,
+    'avg_spawns': 0,
+    'spawn_time': 'N/A',
+    'multipliers': null,
+    'weaknesses': [
+      'Fire',
+      'Electric',
+      'Rock',
+      'Steel'
+    ]
+  }
+];
 
-describe('example', () => {
-  it('debería ser una función', () => {
-    assert.equal(typeof example, 'function');
+const outputSearch = [
+  {
+    'id': 41,
+    'num': '041',
+    'name': 'Zubat',
+    'img': 'http://www.serebii.net/pokemongo/pokemon/041.png',
+    'type': [
+      'Poison',
+      'Flying'
+    ],
+    'egg': '2 km',
+    'spawn_chance': 6.52,
+    'avg_spawns': 652,
+    'spawn_time': '12:28',
+    'multipliers': [
+      2.6,
+      3.67
+    ],
+    'weaknesses': [
+      'Electric',
+      'Ice',
+      'Psychic',
+      'Rock'
+    ],
+  }, {
+    'id': 145,
+    'num': '145',
+    'name': 'Zapdos',
+    'img': 'http://www.serebii.net/pokemongo/pokemon/145.png',
+    'type': [
+      'Electric',
+      'Flying'
+    ],
+    'egg': 'Not in Eggs',
+    'spawn_chance': 0,
+    'avg_spawns': 0,
+    'spawn_time': 'N/A',
+    'multipliers': null,
+    'weaknesses': [
+      'Ice',
+      'Rock'
+    ]
+  }
+];
+
+describe('searchPokemons', () => {
+  it('debería retornar un array con los pokemones cuyo nombre empiecen con z', () => {
+    expect(searchPokemons(data, 'z')).toEqual(outputSearch);
   });
+});
 
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
+const outputSortSpawn = [
+  {
+    'id': 145,
+    'num': '145',
+    'name': 'Zapdos',
+    'img': 'http://www.serebii.net/pokemongo/pokemon/145.png',
+    'type': [
+      'Electric',
+      'Flying'
+    ],
+    'egg': 'Not in Eggs',
+    'spawn_chance': 0,
+    'avg_spawns': 0,
+    'spawn_time': 'N/A',
+    'multipliers': null,
+    'weaknesses': [
+      'Ice',
+      'Rock'
+    ]
+  }, {
+    'id': 144,
+    'num': '144',
+    'name': 'Articuno',
+    'img': 'http://www.serebii.net/pokemongo/pokemon/144.png',
+    'type': [
+      'Ice',
+      'Flying'
+    ],
+    'egg': 'Not in Eggs',
+    'spawn_chance': 0,
+    'avg_spawns': 0,
+    'spawn_time': 'N/A',
+    'multipliers': null,
+    'weaknesses': [
+      'Fire',
+      'Electric',
+      'Rock',
+      'Steel'
+    ]
+  }, {
+    'id': 42,
+    'num': '042',
+    'name': 'Golbat',
+    'img': 'http://www.serebii.net/pokemongo/pokemon/042.png',
+    'type': [
+      'Poison',
+      'Flying'
+    ],
+    'egg': 'Not in Eggs',
+    'spawn_chance': 0.42,
+    'avg_spawns': 42,
+    'spawn_time': '02:15',
+    'multipliers': null,
+    'weaknesses': [
+      'Electric',
+      'Ice',
+      'Psychic',
+      'Rock'
+    ],
+    'prev_evolution': [{
+      'num': '041',
+      'name': 'Zubat'
+    }]
+  }, {
+    'id': 41,
+    'num': '041',
+    'name': 'Zubat',
+    'img': 'http://www.serebii.net/pokemongo/pokemon/041.png',
+    'type': [
+      'Poison',
+      'Flying'
+    ],
+    'egg': '2 km',
+    'spawn_chance': 6.52,
+    'avg_spawns': 652,
+    'spawn_time': '12:28',
+    'multipliers': [
+      2.6,
+      3.67
+    ],
+    'weaknesses': [
+      'Electric',
+      'Ice',
+      'Psychic',
+      'Rock'
+    ],
+  }
+];
+
+describe('sortSpawn', () => {
+  it('debería retornar un array con los pokemones ordenados por propiedad spawn_chance', () => {
+    expect(sortSpawn(data)).toEqual(outputSortSpawn);
+  });
+});
+
+describe('filterWeakness', () => {
+  it('debería retornar un array con los pokemones ordenados por debilidad', () => {
+    expect(filterWeakness(data, 'Electric')[0].weaknesses).toEqual(['Fire', 'Electric', 'Rock', 'Steel']);
+  });
+});
+
+describe('filterTypes', () => {
+  it('debería retornar un array con los pokemones ordenados por tipo', () => {
+    expect(filterTypes(data, 'Poison')[0].type).toEqual(['Poison', 'Flying']);
+  });
+});
+
+const outputEgg = [
+  {
+    'id': 41,
+    'num': '041',
+    'name': 'Zubat',
+    'img': 'http://www.serebii.net/pokemongo/pokemon/041.png',
+    'type': [
+      'Poison',
+      'Flying'
+    ],
+    'egg': '2 km',
+    'spawn_chance': 6.52,
+    'avg_spawns': 652,
+    'spawn_time': '12:28',
+    'multipliers': [
+      2.6,
+      3.67
+    ],
+    'weaknesses': [
+      'Electric',
+      'Ice',
+      'Psychic',
+      'Rock'
+    ],
+  }
+];
+
+describe('filterEggs', () => {
+  it('debería retornar un array con los pokemones ordenados por el tipo de huevo', () => {
+    expect(filterEggs(data, '2 km')).toEqual(outputEgg);
+  });
+});
+
+describe('sortAlfa', () => {
+  it('debería retornar un array con los pokemones ordenados alfabeticamente', () => {
+    expect(sortAlfa(data, '0')[0].name).toEqual('Articuno');
   });
 });
